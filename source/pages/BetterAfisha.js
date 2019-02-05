aa13496f669ef4e1829c64b6aba190145449e777
Anton Rozdobudko <arnygora@gmail.com>
2019-02-05 21:14:05 +0200

    fh

---
+++ source/pages/BetterAfisha.js
@@ -0,0 +1,82 @@
+​// Core
+import React, { useState, useEffect } from 'react';
+
+// Instruments
+import { getStyles } from '../helpers';
+import { api } from '../API';
+
+export const Kinoafisha = () => {
+    const [ selectedFilter, setSelectedFilter ] = useState('upcoming');
+    const [ movies, setMovies ] = useState([]);
+
+    console.log('→ movies', movies);
+
+    const _getMoviesByFilter = async (nextFilter) => {
+        const movies = await api.getMovies(nextFilter);
+
+        setMovies(movies);
+    };
+
+    const _setSelectedFilter = (event) => {
+        const nextFilter = event.currentTarget.dataset.name;
+
+        setSelectedFilter(nextFilter);
+        _getMoviesByFilter(nextFilter);
+    };
+
+    // componentDidMount + componentWillUnmount
+    useEffect(() => {
+        _getMoviesByFilter(selectedFilter);
+    }, []);
+
+    const styles = getStyles({
+        selectedFilter,
+    });
+
+    const moviesJSX = movies.map((movie) => {
+        return (
+            <div
+                className = 'movie'
+                key = { movie.id }>
+                <div className = 'poster'>
+                    <span className = 'genre'>{movie.genre}</span>
+                    <img src = { movie.poster } />
+                    <span className = 'rating'>{movie.rating}</span>
+                </div>
+                <span className = 'title'>{movie.title}</span>
+            </div>
+        );
+    });
+
+    return (
+        <>
+            <div className = 'header'>
+                <div className = 'logo' />
+                <div className = 'filters'>
+                    <div
+                        className = { styles.latestFilter }
+                        data-name = 'latest'
+                        onClick = { _setSelectedFilter }>
+                        <span>Новинки {new Date().getFullYear()}</span>
+                    </div>
+                    <div
+                        className = { styles.upcomingFilter }
+                        data-name = 'upcoming'
+                        onClick = { _setSelectedFilter }>
+                        <span>Скоро в кинотеатрах</span>
+                    </div>
+                    <div
+                        className = { styles.popularFilter }
+                        data-name = 'popular'
+                        onClick = { _setSelectedFilter }>
+                        <span>В топ-чартах</span>
+                    </div>
+                </div>
+            </div>
+            <div className = 'content'>{moviesJSX}</div>
+        </>
+    );
+};
+
+// Nataliia Khitsan​тобто краще було selectedFilter передати другим аргументом в
+// useEffect, щоб вручну не дьоргати api на onClick
